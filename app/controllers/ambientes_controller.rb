ec1144fae2313f5be091f09eb7999257d92de51f
Neto Barbosa <jbsn@cin.ufpe.br>
2016-05-22 14:09:07 +0000

    Atualização de paginas e inclusão de layout

--- app/controllers/ambientes_controller.rb
+++ app/controllers/ambientes_controller.rb
@@ -1,6 +1,11 @@
 class AmbientesController < ApplicationController
   before_action :set_ambiente, only: [:show, :edit, :update, :destroy]
-
+  before_filter :set_menu
+  
+  def set_menu
+    @M_ambiente = "active"
+  end
+  
   # GET /ambientes
   # GET /ambientes.json
   def index
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  before_filter :set_menu
+  
+  def set_menu
+    @M_home = "active"
+  end
+  
   def index
     
   end
