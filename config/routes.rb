e39dc92af7a5887ad4d232ab5a271515733dff80
pedroclericuzi1 <pedro.vinicius.batista@gmail.com>
2016-05-22 15:28:00 +0000

    Commitando a primeira versao do login

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  
+  get '/new' => 'ambientes#new'
+  get '/ambientes/index' => 'ambientes#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
