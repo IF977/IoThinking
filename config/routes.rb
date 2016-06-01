1cb4ef29847832b7eca9ba3938015e8999ebaf21
pedroclericuzi1 <pedro.vinicius.batista@gmail.com>
2016-06-01 22:52:50 +0000

    Resolvendo conflito 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :clientes
   resources :ambientes
 
-<<<<<<< HEAD
   # You can have the root of your site routed with "root"
   
   root 'welcome#index'
@@ -14,15 +13,9 @@ Rails.application.routes.draw do
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-=======
-  root 'static_pages#home'
->>>>>>> d8d82aafc431a1776c5cd7e3ae12d875249984af
 
   get     'new'           =>  'ambientes#new'
   get     'ambientes'     =>  'ambientes#index'
-  get     'sobre'         =>  'static_pages#sobre'
-  get     'contato'       =>  'static_pages#contato'
-  get     'tutorial'      =>  'static_pages#tutorial'
   get     'enviar_senha'  =>  'users#password#new'
   delete  'logout'        =>  'clientes#destroy'
 end
