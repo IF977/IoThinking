108e6d9073e8cd3105977cdd7416515ef7ad1210
pedroclericuzi1 <pedro.vinicius.batista@gmail.com>
2016-06-19 06:48:11 +0000

    alterado o index e o menu

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if user_signed_in?
       redirect_to "/menu_index"
+    else
+      redirect_to "/users/sign_in"
     end
   end
 end
