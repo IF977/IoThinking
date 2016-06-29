f95aca86ede1ea1b0fe746fb0e131848acb7f45d
PedroClericuzi <pedro.vinicius.batista@gmail.com>
2016-06-29 15:11:13 -0300

    fim

--- TesteLiga/TesteLiga.ino
+++ TesteLiga/TesteLiga.ino
@@ -1,39 +1,62 @@
+#include <SD.h>
 #include <SPI.h>
 #include <Ethernet.h>
 
-int LED_RED = 52;
-int LED_WT = 48;
+#include "EmonLib.h"                   // Include Emon Library
+EnergyMonitor emon1;
+const int chipSelect = 4;
 
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 
 //Para conexão com a shield ethernet eu preciso capturar o ip, gateway e subnet 
-IPAddress ip(192,168,1,14);
-IPAddress gateway(192,168,1,1);
+//Rede Casa
+//ip: 192,168,25,36
+//gateway: 192,168,25,1
+//subnet: 255,255,255,0
+
+//Rede BEPiD
+//ip:172,16,1,197
+//gateway:172,16,0,1
+//subnet:255,255,0,0
+
+IPAddress ip(172,16,2,53);
+IPAddress gateway(172,16,0,1);
 IPAddress subnet(255,255,0,0);
 
 //O servidor que eu quero usar no método GET
-char server[] = "http://engenharia-software-pedroclericuzi1.c9users.io";
+char server[] = "http://theiothinking.herokuapp.com";
+unsigned long ltmillis, tmillis, timems, previousMillis;
+
 
 //Status de internet 
 EthernetClient client;
 
 void setup() {
 
-  //Pino e botao
-  //pinMode(botao, INPUT_PULLUP);
-  pinMode(LED_RED, OUTPUT);
-  digitalWrite(LED_RED, LOW);
-  
-  //fim de Pino e botao
+ 
   
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  
+  Serial.print("Initializing SD card...");
+
+  if(!SD.begin(chipSelect)){
+    Serial.println("Card failed, or not present");
+    return;
+  }
+
+  
+  
+  Serial.println("Card initialized");
   //Ethernet.begin(mac, ip);
+  previousMillis = millis();
+
+  emon1.current(1, 61.1);             // Current: input pin, calibration.
   /*// start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
@@ -48,10 +71,38 @@ void setup() {
 }
 
 void loop() {
-  sendGET();
+
+  ltmillis = tmillis;
+  tmillis = millis();
+  timems = tmillis - ltmillis;
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  float kwhTotal =  (((Irms*127.0)/1000.0) * 1.0/3600.0 * (timems/1000.0));
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  
+      dataFile.println(kwhTotal);
+      dataFile.close();
+      sendGET(kwhTotal);
+   
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  /*if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }*/
+  
+  /*// if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }*/
 }
 
-void sendGET()
+void sendGET(float kwhTotal)
 {
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
@@ -67,9 +118,13 @@ void sendGET()
     // Make a HTTP request:
     //client.println("GET /search?q=arduino HTTP/1.1");
     //client.println("Host: www.google.com");
-    //http://projeto-engenharia-iothinking-pedroclericuzi1.c9users.io:8080/ambientes/4
-    client.println("GET /ambientes/?leitura=valor HTTP/1.1");
-    client.println("http://engenharia-software-pedroclericuzi1.c9users.io");
+    char corrente[10];
+    String current;
+    dtostrf(kwhTotal,1,5,corrente);
+    String valor = String (corrente);
+    Serial.println(valor);
+    client.println("GET /ambientes/?leitura="+valor+"  HTTP/1.1");
+    client.println("Host: http://theiothinking.herokuapp.com");
     client.println("Connection: close");
     client.println();
   } else {
