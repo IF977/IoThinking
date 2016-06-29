9cb297d50c5c65dda1dadcacd5492bc4c3d50bef
nascimentosilva <jns2@cin.ufpe.br>
2016-06-29 03:58:33 +0000

    Atualizacao login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :validatable, :authentication_keys => [:login]
+         
+         
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+  
   validates :username,
     :presence => true,
     :uniqueness => {
@@ -16,4 +30,5 @@ class User < ActiveRecord::Base
       errors.add(:username, :invalid)
     end
   end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@ Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   require 'devise/orm/active_record'
-  config.authentication_keys =   [:login ]
+  config.authentication_keys =   [:login]
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
   config.skip_session_storage =  [:http_auth]
